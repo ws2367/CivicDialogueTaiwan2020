8b90e99a79096c2c9251ddd722915ad47322a0cd
Wen Shaw <wenhsiang.shaw@gmail.com>
2019-11-09 17:20:25 -0800

    added p# and line bot

---
+++ line_bot/deploy.sh
@@ -0,0 +1,6 @@
+git push heroku `git subtree split --prefix line_bot master`:master --force
+
+# git subtree split --prefix line_bot master
+# git push heroku 5612dd90d7a6d1046026d6db0cb9057d01d7c819:master --force
+
+# heroku psql HEROKU_POSTGRESQL_BROWN_URL
